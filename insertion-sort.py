8db3e9caa6301611ff27cef7bd7d3cd8d23122de
bthapa99 <90642160+bthapa99@users.noreply.github.com>
2025-01-12 14:55:07 -0500

    This is an initial commit for the file added

---
+++ insertion-sort.py
@@ -0,0 +1,6 @@
+# add logic to insertion sort here.
+def insertion_sort(Arr):
+    return Arr
+    #this method should return sorted Arr instead
+arr = [7,5,2,11,65, 23,33,21]    
+print(insertion_sort(arr))
