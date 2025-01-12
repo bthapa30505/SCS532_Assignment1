b80457e5a02f5cf2f27a379d8c98376378dfdb4a
bthapa99 <90642160+bthapa99@users.noreply.github.com>
2025-01-12 15:00:45 -0500

    Final changes with monotonically descending order

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,14 +1,14 @@
-# This logic sorts items in ascending order.
-# Need to change the logic so that it is in descending order.
+# This logic sorts items in monotonically descending order.
 def insertion_sort(Arr):
     for i in range(1, len(Arr)):
         key = Arr[i]
         j = i - 1
-        while j >= 0 and Arr[j] > key:
+        #Flipping the > to < here to change the ascending order to descending order
+        while j >= 0 and Arr[j] < key:
             Arr[j + 1] = Arr[j]
             j = j - 1
         Arr[j + 1] = key
     return Arr  
 
-arr = [7,5,2,11,65, 23,33,21]    
-print("Sorted array in ascending order:", insertion_sort(arr))
+arr = [7,5,2,11,65, 23,33,21, 11]    
+print("Sorted array in monotonically descending order:", insertion_sort(arr))
